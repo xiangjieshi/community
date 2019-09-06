9db632f5d6e72dcf41bb8dbeb6e1a523378d96dc
shenjingmin <1301414378@qq.com>
2019-09-06 11:11:11 +0800

    init repo

---
+++ src/main/java/com/firstproject/community/controller/hello.java
@@ -0,0 +1,15 @@
+package com.firstproject.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class hello {
+    @GetMapping("/hello")
+    public String hello(@RequestParam("name") String name , Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
